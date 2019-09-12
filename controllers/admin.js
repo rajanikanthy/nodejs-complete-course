6f7cd4aa0ab7d7a8c1ea43124efdec75aaf0f290
R YAMMANURU <ryammanuru@pinterest.com>
2019-09-12 15:38:34 -0700

    Add forms for player and team

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,16 @@
-exports.createTeam = (req, res, next) => {
+exports.showCreateTeamView = (req, res, next) => {
     res.render('create-team', { title : 'Create Team'});
 }
 
-exports.addPlayer = (req, res, next) => {
+exports.createTeam = (req, res, next) => {
+    console.log('Creating team');
+    res.redirect('/teams');
+}
+
+exports.showCreatePlayerView = (req, res, next) => {
     res.render('add-player', { title : 'Add Player'})
+}
+
+exports.createPlayer = (req, res, next) => {
+    res.redirect('/players');
 }
--- routes/admin.js
+++ routes/admin.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const adminController = require('../controllers/admin');
 
-router.get('/teams/create-team', adminController.createTeam);
-router.get('/players/add-player', adminController.addPlayer);
+router.get('/teams/show-create-team-view', adminController.showCreateTeamView);
+router.post('/teams/create-team', adminController.createTeam);
+router.get('/players/show-add-player-view', adminController.showCreatePlayerView);
+router.post('/players/add-player', adminController.createPlayer)
 
 module.exports = router;
